3f6244f20dbf084690e5eee070f4a36a5f7453c5
quanwei li <quanwei li>
2017-04-13 21:05:33 +0800

    remove custom config

--- init.el
+++ init.el
@@ -169,7 +169,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/org/trello.org" "~/org/task.org")))
+ '(org-agenda-files nil)
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
  '(org-trello-files (quote ("~/org/trello.org")) nil (org-trello))
  '(package-selected-packages
